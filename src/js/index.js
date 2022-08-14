2941823712a319aad7d7bd972b07bb9eb73b2fca
lraulc <lraul.castillo@gmail.com>
2022-08-14 14:34:53 -0500

    responsive page, DOM buttons and icon

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,26 @@
 import "../SCSS/styles.scss";
 import * as bootstrap from "bootstrap";
+
+const topBtn = document.getElementById("top-btn");
+const resetBtn = document.getElementById("reset-btn");
+
+let counter = 0;
+const clickCount = document.getElementById("click-counter");
+
+topBtn.addEventListener("mousedown", () => {
+    topBtn.innerHTML = "Clicked";
+    counter += 1;
+    clickCount.innerHTML = counter;
+    if (counter > 0) {
+        clickCount.style.color = "orange";
+    }
+});
+topBtn.addEventListener("mouseup", () => {
+    topBtn.innerHTML = "Click me";
+});
+
+resetBtn.addEventListener("click", () => {
+    counter = 0;
+    clickCount.innerHTML = counter;
+    clickCount.style.color = "white";
+});
