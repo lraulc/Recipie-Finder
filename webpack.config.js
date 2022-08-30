b214e98be3b45249963203781fc2746e2effa50e
Angel123Lara <adjls@gmail.com>
2022-08-30 00:14:57 -0500

    Fix src

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.m?js$/,
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
