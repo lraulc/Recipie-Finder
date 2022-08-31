dc6cde0c48a205d5a710e77ba301988bbbdff10a
Silvia rodriguez <silvideastora18@gmail.com>
2022-08-31 16:43:18 -0500

    Created random recipie UI and logic

--- src/js/recipie-detail.js
+++ src/js/recipie-detail.js
@@ -1,3 +1,4 @@
+
 //Esto sirve para ejecutar la petición cuando la pagina carga
 function init() {
     const mealID = location.search.substring(4);
@@ -12,9 +13,36 @@ function init() {
 init()
 
 //Para renderizar el contenido de la peticion
+const singleIngredient = document.getElementById('single-ingredient');
+
 function addMealToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <=10; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+  singleIngredient.innerHTML= `
+    <ul>
+      ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+    </ul>`;
+
+
     const recipieImage = document.getElementById('recipie-image');
     const category = document.getElementById('category');
+    const area = document.getElementById('area')
+    const recipieTitle = document.getElementById('recipie-title');
+    const  recipieInstructions= document.getElementById('recipie-instructions');
+
     recipieImage.src = meal.strMealThumb;
     category.innerText = meal.strCategory;
+    area.innerText = meal.strArea;
+    recipieTitle.innerText = meal.strMeal;
+    recipieInstructions.innerText = meal.strInstructions;
+
 }
