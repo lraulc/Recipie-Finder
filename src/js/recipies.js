86ad2659f079a0157c268714b37fc03e6b98edb5
Angel123Lara <adjls@gmail.com>
2022-09-01 13:55:50 -0500

    Se visuaiza imagen y se agrego URL

--- src/js/recipies.js
+++ src/js/recipies.js
@@ -1,4 +1,4 @@
-let aux= localStorage.getItem("key1")
+let aux = localStorage.getItem("key1")
 console.log(aux)
 
 //======= variables para url
@@ -8,80 +8,150 @@ console.log(completeUrl)
 // === variables para manipular el resultado
 let nameRecipie = []
 let UrlRecipieimg = []
-
+let idRecipe = []
 
 let idSearch = document.getElementById("search")
-let nodeSearch = document.createElement("strong")
-let textsearch = document.createTextNode(aux)
-nodeSearch.appendChild(textsearch)
-console.log(nodeSearch)
 idSearch.removeChild(idSearch.lastChild)
-idSearch.appendChild(nodeSearch)
-console.log(idSearch)
-
-// Test
-
-function ChangeContainer(data){
-  for (let i = 0; i < 7 ; i++) {
-    let containerRecipie = document.getElementById("idcontainer")
-    
-  }
-}
-
-
-
+idSearch.appendChild(CreateNode("strong", aux))
 
 
 getdata(completeUrl)
   .then(data => {
-    console.log(data.meals)
-    console.log('resultado')
-    console.log('process')
-    objToArray(data.meals)
-    //console.log(nameRecipie)
-    //console.log(UrlRecipieimg)
+
+    let resp = managedata(data)  /// Revisa que el resultado tenga al menos un valor como respuesta
+    let IdContainerMain = document.getElementById("idcontainer1")
+    let childesContainer = IdContainerMain.childNodes
+    let Btn_contenedor = document.getElementsByClassName('contenedorBoton')
+    
+    let IdNumRecipie = document.getElementById("results")
+
+
+    if (resp == "error") {
+      IdContainerMain.style.display = "none"
+      IdContainerMain.parentNode.appendChild(CreateNode('h1', 'Elemento no encontrado'))
+      return
+    }
+    // coloca el numero de recetas encontradas
+    let numResults = nameRecipie.length.toString() + " recipies"
+    
+    IdNumRecipie.removeChild(IdNumRecipie.lastChild)
+    IdNumRecipie.appendChild(CreateNode("strong",numResults))
+
+    for (let i = 0; i < childesContainer.length; i++) {
+
+      ModifCard(childesContainer[i], 0)
+
+    }
+    if (nameRecipie.length > 1) {
+      for (let h = 1; h < nameRecipie.length; h++) {
+        let dupnode = IdContainerMain.cloneNode(true)
+        IdContainerMain.parentNode.appendChild(dupnode)
+        let nwChilds = IdContainerMain.parentNode.lastChild.childNodes
+
+        for (let g = 0; g < nwChilds.length; g++) {
+
+          ModifCard(nwChilds[g], h)
+
+        }
+
+      }
+    }
+    for (let j = 0; j < (Btn_contenedor.length - 1); j++) {
+      let ChildBtn = Btn_contenedor[j].childNodes
+      for (let g = 0; g < ChildBtn.length; g++) {
+        if(ChildBtn[g].href=!undefined){
+          ChildBtn[g].href = "/recipie-detail.html?id="+idRecipe[j]
+          console.log(ChildBtn[g].href)
+        }
+        
+      }
+      
+    }
+      
+    
+    
 
   }
- )
-
-  
-  
 
 
+  )
+
+// funcion principal
+
+function managedata(data) {
+  console.log(data.meals)
+  if (data.meals != null) {
+    return objToArray(data.meals)
+  } else {
+
+    return "error"
+  }
 
 
 
-  //// === funciones 
-async function getdata(completeUrl){
-  try{
-
-  return fetch(completeUrl)
-  .then(function (response) {
-    if (response.status==404){
-      return ('error')
-    }else{
-      console.log('===')
-      
-      return response.json();
-  }})
-  .then(function (data) {
-    console.log('obt data')
-    return data;
-  })
-}
-catch{
-  console.log(error)
-}
+  //console.log(nameRecipie)
+  //console.log(UrlRecipieimg)
 }
 
-function objToArray(arrayData){
-  arrayData.forEach(element => {
-    nameRecipie.push(element.strMeal)
-    UrlRecipieimg.push(element.strMealThumb)
-  });
+//// === funciones 
+async function getdata(completeUrl) {
+  try {
+
+    return fetch(completeUrl)
+      .then(function (response) {
+        if (response.status == 404) {
+          return ('error')
+        } else {
+          console.log('===')
+
+          return response.json();
+        }
+      })
+      .then(function (data) {
+        console.log('obt data')
+        return data;
+      })
+  }
+  catch {
+    console.log(error)
+  }
+}
+
+function objToArray(arrayData) {
+  if (arrayData.length != 0) {
+    arrayData.forEach(element => {
+      nameRecipie.push(element.strMeal)
+      UrlRecipieimg.push(element.strMealThumb)
+      idRecipe.push(element.idMeal)
+    })
+  } else {
+    console.log('Error')
+  };
   ;
 }
 
+// funcion 
+function CreateNode(element, text) {
+  let node = document.createElement(element)
+  let textnode = document.createTextNode(text)
+  return node.appendChild(textnode)
+
+
+}
+
+function ModifCard(childnode, j) {
+  if (childnode.className == "javaScript") {
+    childnode.appendChild(document.createTextNode(nameRecipie[j]))
+    childnode.removeChild(childnode.firstChild)
+
+  }
+  if (childnode.src != null) {
+    childnode.src = UrlRecipieimg[j]
+    
+  };
+
+
+}
 
 
 
